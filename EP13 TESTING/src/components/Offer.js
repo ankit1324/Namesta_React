e5a36af9144548953bdcfb5eef68f087f9777934
Ankit <ak0330255@gmail.com>
2024-03-17 17:28:56 +0530

    offer tests

--- EP13 TESTING/src/components/Offer.js
+++ EP13 TESTING/src/components/Offer.js
@@ -1,8 +1,28 @@
 const Offer = () => {
   return (
     <>
-      <div className="container">
-        <h1>Offer</h1>
+      <div className="container bg-yellow-50 text-center">
+        <h1 className="m-4 p-4 font-bold text-2xl">Offer</h1>
+        <form action="">
+          <input
+            type="text"
+            className="border border-black"
+            placeholder="offer"
+          />
+
+          <button
+            type="submit"
+            className="ml-4 p-1 rounded-xl border border-black"
+          >
+            Submit
+          </button>
+          <input
+            name="2"
+            type=""
+            className="border ml-2 border-black"
+            placeholder="offers"
+          />
+        </form>
       </div>
     </>
   );
